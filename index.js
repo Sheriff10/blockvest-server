22fb280a904fd1dd1f78853a99205fb062925512
sheriff ibrahim <ibrahimsheriff999@gmail.com>
2023-11-30 00:26:50 +0100

    added cors config

--- index.js
+++ index.js
@@ -12,11 +12,17 @@ mongoose
    .catch((err) => console.log("Database Connection Error: " + err));
 
 const app = express();
-app.use(cors());
+app.use(
+   cors({
+      origin: "*", // Allow requests from any origin
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow the specified methods
+      credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+   })
+);
 app.use(express.json());
 require("./start/routeModule")(app);
 
-app.use(error)
+app.use(error);
 
 app.listen(5000 || process.env.PORT, () => {
    console.log("listening to port 5000");
